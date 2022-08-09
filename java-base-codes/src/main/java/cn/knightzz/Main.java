5859e628cc1eded86c83622de5845d7d7e6192ee
王天赐 <15565946702@163.com>
2022-08-09 08:47:53 +0800

    init commit

---
+++ java-base-codes/src/main/java/cn/knightzz/Main.java
@@ -0,0 +1,16 @@
+package cn.knightzz;
+
+/**
+ * @author 王天赐
+ * @title: ${NAME}
+ * @projectName ${PROJECT_NAME}
+ * @description: ${description}
+ * @website <a href="http://knightzz.cn/">http://knightzz.cn/</a>
+ * @github <a href="https://github.com/knightzz1998">https://github.com/knightzz1998</a>
+ * @create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
